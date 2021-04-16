49d012f552511b7c48e08c0154f2fb8f591dc9a7
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-16 18:23:11 +0300

    Allow fetch pictures without main.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -4,6 +4,11 @@ from urllib.parse import unquote, urlsplit
 import requests
 
 
+def ensure_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
 def parse_file_ext(url):
     unquoted_link = unquote(url)
     link = urlsplit(unquoted_link)
@@ -30,3 +35,13 @@ def fetch_spacex_last_launch():
             image,
             "spacex{}{}".format(counter+1, parse_file_ext(image)),
         )
+
+
+def main():
+    ensure_dir("./images/")
+
+    fetch_spacex_last_launch()
+
+
+if __name__ == '__main__':
+    main()
