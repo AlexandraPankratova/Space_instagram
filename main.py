c92e979a5c36cdfc693a599409f70ecebb14102a
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-16 11:42:17 +0300

    Create def picture_download()

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 import os
 
+
 def ensure_dir(dir_name):
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
 
-def main():
-    ensure_dir("./images/")
-    filename = "./images/hubble.jpeg"
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+def picture_download(url, picture_name):
+    filename = "./images/{}".format(picture_name)
 
     response = requests.get(url)
     response.raise_for_status()
@@ -17,8 +17,13 @@ def main():
         file.write(response.content)
 
 
+def main():
+    ensure_dir("./images/")
+    picture_download(
+        "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg",
+        "hubble.jpeg",
+                     )
+
 
 if __name__ == '__main__':
     main()
-
-
