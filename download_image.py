04b9a29bc42764e1b0a67a176d888ca3ba94392c
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-19 13:57:38 +0300

    Create new file for repeating functions

---
+++ download_image.py
@@ -0,0 +1,25 @@
+import os
+from urllib.parse import unquote, urlsplit
+
+import requests
+
+
+def ensure_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
+def parse_file_ext(url):
+    unquoted_link = unquote(url)
+    link = urlsplit(unquoted_link)
+    return os.path.splitext(link.path)[1]
+
+
+def download_image(url, image_name):
+    filename = "./images/{}".format(image_name)
+
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
